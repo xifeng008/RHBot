12f6a1a5494907e27dcdb7241c38d4fd7a756c4b
xifeng008 <75963961+xifeng008@users.noreply.github.com>
2024-02-05 06:08:28 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/page.tsx
@@ -0,0 +1,41 @@
+import { getFrameMetadata } from '@coinbase/onchainkit';
+import type { Metadata } from 'next';
+import { NEXT_PUBLIC_URL } from './config';
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: 'Tell me the story',
+    },
+    {
+      label: 'Redirect to cute dog pictures',
+      action: 'post_redirect',
+    },
+  ],
+  image: `${NEXT_PUBLIC_URL}/park-1.png`,
+  input: {
+    text: 'Tell me a boat story',
+  },
+  post_url: `${NEXT_PUBLIC_URL}/api/frame`,
+});
+
+export const metadata: Metadata = {
+  title: 'zizzamia.xyz',
+  description: 'LFG',
+  openGraph: {
+    title: 'zizzamia.xyz',
+    description: 'LFG',
+    images: [`${NEXT_PUBLIC_URL}/park-1.png`],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>zizzamia.xyz</h1>
+    </>
+  );
+}
