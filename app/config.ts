842285742b57adc529eeafb7b1403f2080958c12
pengfan <450652673@qq.com>
2024-05-28 17:05:42 +0800

    test

--- app/config.ts
+++ app/config.ts
@@ -1,2 +1,2 @@
 // export const NEXT_PUBLIC_URL = 'https://www.boostguild.xyz/';
-export const NEXT_PUBLIC_URL = 'https://boostguild.xyz/llama/optimism/actions/72'
+export const NEXT_PUBLIC_URL = ' https://app.llama.xyz/orgs/boost/optimism/actions/72'
