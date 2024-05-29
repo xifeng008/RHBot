a11df18fc3554456edf18fafee6d4ef20628d85f
pengfan <450652673@qq.com>
2024-05-29 09:43:41 +0800

    fix: test

--- app/llama/optimism/test/page.tsx
+++ app/llama/optimism/test/page.tsx
@@ -1,6 +1,6 @@
 import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
-import { NEXT_PUBLIC_URL } from '../../../config';
+import { NEXT_PUBLIC_URL, NEXT_PUBLIC_POST_URL } from '../../../config';
 
 const frameMetadata = getFrameMetadata({
   buttons: [
@@ -13,7 +13,7 @@ const frameMetadata = getFrameMetadata({
   ],
   image: `${NEXT_PUBLIC_URL}/boost-pass-display.png`,
   // post_url: `${NEXT_PUBLIC_URL}/api/frame`,
-  post_url: `${NEXT_PUBLIC_URL}`,
+  post_url: `${NEXT_PUBLIC_POST_URL}`,
 });
 
 export const metadata: Metadata = {
