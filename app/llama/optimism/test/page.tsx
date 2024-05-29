747496cc0ea87f8a7ba7c027cf6a2cfcd3f9344f
pengfan <450652673@qq.com>
2024-05-29 09:40:15 +0800

    fix: test

---
+++ app/llama/optimism/test/page.tsx
@@ -0,0 +1,38 @@
+import { getFrameMetadata } from '@coinbase/onchainkit';
+import type { Metadata } from 'next';
+import { NEXT_PUBLIC_URL } from '../../../config';
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: 'Read Summary',
+    },
+    {
+      label: 'Go To Approved',
+    }
+  ],
+  image: `${NEXT_PUBLIC_URL}/boost-pass-display.png`,
+  // post_url: `${NEXT_PUBLIC_URL}/api/frame`,
+  post_url: `${NEXT_PUBLIC_URL}`,
+});
+
+export const metadata: Metadata = {
+  title: 'Boot Guild!',
+  description: 'LFG',
+  openGraph: {
+    title: 'RH',
+    description: 'LFG',
+    images: [`${NEXT_PUBLIC_URL}/park-1.png`],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>Boost Guild!</h1>
+    </>
+  );
+}
