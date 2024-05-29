4c0d42f889bcd602640930912f28654b428a9f41
pengfan <450652673@qq.com>
2024-05-29 09:34:12 +0800

    fix: test

--- app/config.ts
+++ app/config.ts
@@ -1,2 +1,3 @@
-// export const NEXT_PUBLIC_URL = 'https://www.boostguild.xyz/';
-export const NEXT_PUBLIC_URL = 'https://app.llama.xyz/orgs/boost/optimism/actions/72'
+export const NEXT_PUBLIC_URL = 'https://www.boostguild.xyz';
+export const NEXT_PUBLIC_POST_URL = `${NEXT_PUBLIC_URL}/llama/optimism/actions/72`
+
