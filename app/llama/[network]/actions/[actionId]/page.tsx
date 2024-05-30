d4048d3af5e329c931df24937f3df435dbb392fd
pengfan <450652673@qq.com>
2024-05-30 14:30:43 +0800

    fix: test

--- app/llama/[network]/actions/[actionId]/page.tsx
+++ app/llama/[network]/actions/[actionId]/page.tsx
@@ -1,23 +1,33 @@
 import { FrameMetadata } from '@coinbase/onchainkit';
-import type { Metadata } from 'next';
 import { NEXT_PUBLIC_URL } from '../../../../config';
+import { NextRequest, NextResponse } from 'next/server'
 
-// export const metadata: Metadata = {
-//   title: 'Boot Guild!',
-//   description: 'LFG',
-//   openGraph: {
-//     title: 'RH',
-//     description: 'LFG',
-//     images: [`${NEXT_PUBLIC_URL}/park-1.png`],
-//   },
-//   other: {
-//     // ...frameMetadata,
-//   },
-// };
+export async function POST(req: NextRequest): Promise<Response> {
+  const searchParams = req.nextUrl.searchParams
+  const actionId = searchParams.get("actionId")
+  const network = searchParams.get("network")
+  const data = await req.json()
+  const buttonId = data.untrustedData.buttonIndex
+  let path: string;
+  if (buttonId == 1) {
+    path = `${NEXT_PUBLIC_URL}/llama/${network}/actions/${actionId}`
+  } else if (buttonId == 2) {
+    // path = `https://app.llama.xyz/orgs/boost/${network}/actions/${actionId}/${req.url}/${req.nextUrl}`
+    path = `https://${req.url}/${req.nextUrl}`
+  } else {
+    path = ""
+  }
 
+  const headers = new Headers()
+  headers.set('Loaction', NEXT_PUBLIC_URL)
+  const response = NextResponse.redirect(`${path}`, {
+    headers,
+    status: 302
+  })
+  return response
+}
 
-
-
+export const dynamic = "force-dynamic"
 
 export default function Page() {
   const network = "optimism"
