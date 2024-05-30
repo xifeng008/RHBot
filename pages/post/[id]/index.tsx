5b94bd22a794c2a842f01e3ac406c853af8dc667
pengfan <450652673@qq.com>
2024-05-30 14:53:39 +0800

    fix: test

--- pages/post/[id]/index.tsx
+++ pages/post/[id]/index.tsx
@@ -1,6 +1,21 @@
 import { FrameMetadata } from '@coinbase/onchainkit';
 import { NEXT_PUBLIC_URL } from '../../../app/config';
 import { useRouter } from 'next/router'
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Boot Guild!',
+    description: 'LFG',
+    openGraph: {
+        title: 'RH',
+        description: 'LFG',
+        images: [`${NEXT_PUBLIC_URL}/park-1.png`],
+    },
+    other: {
+
+    },
+};
+
 
 export default function Page() {
     const network = "optimism"
