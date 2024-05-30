27ef26cb0dd1645bea914c2fb1d4437d4bbda7f0
pengfan <450652673@qq.com>
2024-05-30 15:01:03 +0800

    fix: test

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -5,15 +5,15 @@ export async function POST(req: NextRequest): Promise<Response> {
     // const searchParams = req.nextUrl.searchParams
     // const actionId = searchParams.get("actionId")
     // const network = searchParams.get("network")
+    const actionId = 72
+    const network = 'optimism'
     const data = await req.json()
     const buttonId = data.untrustedData.buttonIndex
     let path: string;
     if(buttonId == 1) {
-        path = ''
-        path = `${NEXT_PUBLIC_URL}/llama/op/actions/1`
+        path = `${NEXT_PUBLIC_URL}/llama/${network}/actions/${actionId}`
     } else if(buttonId == 2) {
-        // path = `https://app.llama.xyz/orgs/boost/${network}/actions/${actionId}/${req.url}/${req.nextUrl}`
-        path = `https://${req.url}/${req.nextUrl}`
+        path = `https://app.llama.xyz/orgs/boost/${network}/actions/${actionId}`
     } else {
         path = ""
     }
