071ae8e86874d2f268605b44e8554ae0a9be14bb
pengfan <450652673@qq.com>
2024-05-31 17:25:48 +0800

    fix: 删除多余得第三方库

--- app/llama/[network]/actions/[actionId]/page.tsx
+++ app/llama/[network]/actions/[actionId]/page.tsx
@@ -1,38 +1,17 @@
-"use client";
-
 import { FrameMetadata } from '@coinbase/onchainkit';
 import { NEXT_PUBLIC_URL } from '@/app/config';
 
 import { NETWORK_TO_CHAIN_ID, NetworkName } from '@/lib/network';
 import { getActionInfo } from '@/service/getInfoAction';
 import BoostImage from '@/app/components/BoostImage/BoostImage';
-import * as htmlToImage from 'html-to-image';
-import { useEffect, useState } from 'react';
 
 
-
-
-export default function Page({ params }: { params: { network: NetworkName, actionId: string } }) {
-
-  const [imgUrl, setImgUrl] = useState<string>("");
+export default async function Page({ params }: { params: { network: NetworkName, actionId: string } }) {
 
   // 根据network name 获取 chainId
   const chainId = NETWORK_TO_CHAIN_ID[params.network]
 
-  useEffect(() => {
-    async function fetchData() { 
-      const actionInfo = await getActionInfo(params.actionId, chainId)
-      console.log(actionInfo)
-
-      const boostImg = document.querySelector("#boost-image-html") as HTMLElement;
-      const dataUrl = await htmlToImage.toPng(boostImg, { width: 1200, height: 630})
-      setImgUrl(dataUrl)
-    }
-
-    fetchData()
-    
-  }, [])
-
+  const actionInfo = await getActionInfo(params.actionId, chainId)
 
   return (
     <>
@@ -50,7 +29,7 @@ export default function Page({ params }: { params: { network: NetworkName, actio
           ]
         }
         image={`${NEXT_PUBLIC_URL}/boost-pass-disaplay.png`}
-        post_url={`${NEXT_PUBLIC_URL}/api/frame?network=${params.network}&actionId=${params.actionId}/imgUrl=${imgUrl}`}
+        post_url={`${NEXT_PUBLIC_URL}/api/frame?network=${params.network}&actionId=${params.actionId}`}
       ></FrameMetadata>
       <BoostImage />
     </>
--- service/getInfoAction.ts
+++ service/getInfoAction.ts
@@ -1,18 +1,10 @@
 import axios from "axios"
 
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
-console.log(baseUrl)
-
 // 请求llama接口
 export async function getActionInfo(actionId: string | number, chainId: number) {
-    const url = `api/external/v1/orgs/boost/actions/${actionId}?chainId=${chainId}`;
+    const url = `https://app.llama.xyz/api/external/v1/orgs/boost/actions/${actionId}?chainId=${chainId}`;
     try {
-        const response = await axios({
-            baseURL: baseUrl,
-            url: url,
-            method: 'get',
-            withCredentials: true
-        });
+        const response = await axios.get(url)
         if (response.data.status === 200) {
             return response.data.data;
         }
