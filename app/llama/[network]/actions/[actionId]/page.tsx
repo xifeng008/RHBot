4f956027b9259b4b032353f34d5de1246c2f21ef
pengfan <450652673@qq.com>
2024-06-02 13:57:28 +0800

    fix: test

--- app/llama/[network]/actions/[actionId]/page.tsx
+++ app/llama/[network]/actions/[actionId]/page.tsx
@@ -3,78 +3,9 @@ import { NEXT_PUBLIC_URL } from '@/app/config';
 
 import { NETWORK_TO_CHAIN_ID, NetworkName } from '@/lib/network';
 import { getActionInfo } from '@/service/getInfoAction';
-// import { getBoostGuildHomeHtml } from '@/lib/template';
+import { getBoostGuildHomeHtml } from '@/lib/template';
 import htmlToBase64 from '@/lib/htmlToBase64';
 
-function getBoostGuildHomeHtml(creatorAddress: string, title: string, state: string) {
-  const html_str = `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <title>Title</title>
-      <style>
-          * {
-              margin: 0;
-              padding: 0
-          }
-  
-          ul {
-              list-style: none;
-          }
-          #boost_image_html {
-              position: relative;
-              width: 1200px;
-              height: 630px;
-              box-sizing: border-box;
-              color: #202042;
-              background-color: #fff;
-              padding: 50px 45px 25px;
-          }
-  
-          #boost_image_html .url_text {
-              position: absolute;
-              bottom: 0;
-              right: 0;
-              font-size: 22px;
-  
-          }
-  
-          .boost_logo {
-              width: 431px;
-              height: 78px;
-              margin-bottom: 80px;
-          }
-  
-          .boost_logo img {
-              width: 100%;
-              height: 100%;
-          }
-  
-          .action_info_box > li {
-              font-size: 24px;
-              margin-bottom: 45px;
-          }
-      </style>
-  </head>
-  <body>
-      <div id="boost_image_html">
-          <div class="boost_logo">
-              <img src=${NEXT_PUBLIC_URL + '/images/boostLog.png'} alt="">
-          </div>
-          <ul class="action_info_box">
-              <li>CreatorAddress: ${creatorAddress.slice(0, 5) + "..." + creatorAddress.slice(creatorAddress.length-5)}</li>
-              <li>Title: ${title}</li>
-              <li>State: ${state}</li>
-          </ul>
-          <div class="url_text">boostguild.xyz</div>
-      </div>
-  </body>
-  </html>
-`
-
-return html_str
-}
 
 export default async function Page({ params }: { params: { network: NetworkName, actionId: string } }) {
 
@@ -96,7 +27,7 @@ export default async function Page({ params }: { params: { network: NetworkName,
         buttons={
           [
             {
-              label: `Read Summary12${actionInfo.state}`,
+              label: `Read Summary12${imgUrl}`,
               action: 'post'
             },
             {
@@ -108,6 +39,7 @@ export default async function Page({ params }: { params: { network: NetworkName,
         image={imgUrl}
         post_url={`${NEXT_PUBLIC_URL}/api/frame?network=${params.network}&actionId=${params.actionId}`}
       ></FrameMetadata>
+      <img src={imgUrl} alt="" />
     </>
   );
 }
