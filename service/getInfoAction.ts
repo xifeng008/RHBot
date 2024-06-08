6979b7c61dd3147763e7d70c92102b2415d76aa7
pengfan <450652673@qq.com>
2024-06-08 20:52:32 +0800

    fix: 注释本地测试url地址

--- service/getInfoAction.ts
+++ service/getInfoAction.ts
@@ -2,8 +2,7 @@ import axios from "axios"
 
 // 请求llama接口
 export async function getActionInfo(actionId: string | number, chainId: number, type: string | number) {
-    // const url = `http://23.95.199.59:8888/boost/actionInfo/image?actionId=${actionId}&chainId=${chainId}&type=${type}`;
-    const url = `http://127.0.0.1:5000/boost/actionInfo/image?actionId=${actionId}&chainId=${chainId}&type=${type}`;
+    const url = `http://23.95.199.59:8888/boost/actionInfo/image?actionId=${actionId}&chainId=${chainId}&type=${type}`;
     try {
         const response = await axios.get(url)
         if (response.data.code === 200) {
